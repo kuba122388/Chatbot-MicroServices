d56fe6a5f67ce5654e3207129679ce3e73a0b789
Jacob <245762@edu.p.lodz.pl>
2025-04-19 17:52:18 +0200

    - App.js - Messages are now properly loading every time - docker-compose.yml - Added rabbit message queue - main.py - Implemented queue from rabbit queue image for future chatbot feature - requirements.txt - added pika library in order to make code work

--- api/main.py
+++ api/main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
+from pymongo import MongoClient
+import pika
+import json
+
+client = MongoClient("mongodb://root:example@mongo:27017")
+db = client["mydatabase"]
+print(client.list_database_names())
+dblist = client.list_database_names()
+if "mydatabase" in dblist:
+  print("The database exists.")
+
+messages_collection = db.messages
+
 
 app = FastAPI()
 
@@ -13,17 +26,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Message(BaseModel):
     user: str
     content: str
 
-messages = []
+def send_to_queue(message: dict):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='queue')
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue='chatbot_queue')
+
+    # Przekształcamy wiadomość w JSON
+    body = json.dumps(message)
+
+    # Wysyłamy wiadomość
+    channel.basic_publish(exchange='',
+                          routing_key='chatbot_queue',
+                          body=body)
+
+    print(f"[x] Wysłano do kolejki: {body}")
+    connection.close()
+
 
 @app.get("/messages", response_model=List[Message])
 async def get_messages():
-    return messages
+    messages_cursor = messages_collection.find()
+    messages_list = list(messages_cursor)
+    return [{"user": msg["user"], "content": msg["content"]} for msg in messages_list]
 
 @app.post("/message")
 async def post_message(message: Message):
-    messages.append(message)
+    message_dict = message.dict()
+    messages_collection.insert_one(message_dict)
+
+    message_dict.pop("_id", None)
+    send_to_queue(message_dict)
+
     return {"status": "ok"}
+
